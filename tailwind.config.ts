cf18455fd5fb99e3a208ade662f1a68a7c12e7d6
dureotkd <dureotkd123@naver.com>
2025-02-08 23:08:59 +0900

    반응형 이력서 준비중

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,65 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
+      translate: {
+        center: "-50%",
+      },
+      borderColor: () => ({
+        primary: "#3d444db3",
+      }),
+      textColor: () => ({
+        primary: "#fff",
+        silver200: "#f0f6fc",
+        silver800: "#9198a1",
+      }),
+      backgroundColor: () => ({
+        primary: "#0d1117",
+      }),
+      margin: {
+        tiny: "2px",
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "48px",
+        xxl: "60px",
+        auto: "auto",
+      },
+      padding: {
+        sm: "8px",
+        md: "16px",
+        lg: "24px",
+        xl: "48px",
+        auto: "auto",
+      },
+      fontSize: {
+        xxs: ".5rem",
+        xs: ".75rem",
+        sm: ".875rem",
+        tiny: ".875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+        "7xl": "5rem",
+      },
+      fontFamily: {
+        Pretendard: ["Pretendard"],
+      },
+      container: {
+        padding: {
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
